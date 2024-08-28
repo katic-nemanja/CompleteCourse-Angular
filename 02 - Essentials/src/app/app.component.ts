13febe0e4efc234e50881d3c238014e4b6bc3b9b
katic-nemanja <katic.nemanja@gmail.com>
2024-08-29 00:45:16 +0200

    poseldnje pogledao 39 - Accepting Objects As Inputs Adding Appropriate Typings

--- 02 - Essentials/src/app/app.component.ts
+++ 02 - Essentials/src/app/app.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   userList = DUMMY_USERS;
+  selecteUserId!:any;
+  imeKorisnika!:any;
+
+  get selectedUser(){
+    return this.userList.find(user=>user.id ===this.selecteUserId)?.name!;
+  }
+
+  onClickedListItem(id:string){
+    this.selecteUserId = id;
+    // this.ImeZaTransport = this.userList.find(user=>user.id===id)?.name!;
+  }
 }
