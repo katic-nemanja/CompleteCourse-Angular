dc6817b9c3e48f3ad01d8d5edbcc9f04b1561b13
katic-nemanja <katic.nemanja@gmail.com>
2024-08-29 14:42:21 +0200

    stigao do 41 - Outputting List Content

--- 02 - Essentials/src/app/user/user.component.ts
+++ 02 - Essentials/src/app/user/user.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, Output} from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
 
 @Component({
   selector: 'app-user',
@@ -8,16 +15,18 @@ import { Component, EventEmitter, Input, Output} from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required:true}) id !:string;
-  @Input({required:true}) avatar!:string;
-  @Input({required:true}) name!:string;
+
+  @Input({ required: true }) user!: User;
+  // @Input({ required: true }) id !: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
   @Output() select = new EventEmitter<string>();
 
-  get imagePath(){
-    return '../assets/users/' + this.avatar;
+  get imagePath() {
+    return '../assets/users/' + this.user.avatar;
   }
 
-  onSelectUser(){
-    this.select.emit(this.id)
+  onSelectUser() {
+    this.select.emit(this.user.id)
   }
 }
