e1b9b958fa8e2bf346d09078658782e99d52aaaa
katic-nemanja <katic.nemanja@gmail.com>
2024-09-01 23:40:42 +0200

    Presao sam na folder 04 - Practice time

---
+++ 03 - Working with Modules/src/app/tasks/tasks.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { TasksComponent } from "./tasks.component";
+import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
+import { CommonModule, DatePipe } from "@angular/common";
+import { SharedModule } from "../shared/shared.module";
+import { FormsModule } from "@angular/forms";
+
+@NgModule({
+    declarations:[TasksComponent, TaskComponent, NewTaskComponent],
+    exports:[TasksComponent],
+    imports:[CommonModule, FormsModule, SharedModule]
+})
+export class TasksModule{
+
+}
