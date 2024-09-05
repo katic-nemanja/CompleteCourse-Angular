61418bada102e07ea6f0dcaf46eab89f0716fbfa
katic-nemanja <katic.nemanja@gmail.com>
2024-09-06 00:02:23 +0200

    dodat pipe

--- 04 - Practice time/src/app/investment-results/investment-results.component.ts
+++ 04 - Practice time/src/app/investment-results/investment-results.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Result } from '../shared/result.model';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-investment-results',
   standalone: true,
-  imports: [],
+  imports: [DecimalPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
