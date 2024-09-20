ae83ec4da43ae28ff9785b9bd12626a0b4079efe
katic-nemanja <katic.nemanja@gmail.com>
2024-09-21 00:41:57 +0200

    ispravka strukture

--- 07 - Course Project/src/app/app.module.ts
+++ 07 - Course Project/src/app/app.module.ts
@@ -4,22 +4,24 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import { RecipeListComponent } from './RecipeBook/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './RecipeBook/recipe-item/recipe-item.component';
-import { RecipeDetailsComponent } from './RecipeBook/recipe-details/recipe-details.component';
-import { ShoppingListComponent } from './ShoppingList/shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './ShoppingList/shopping-list-edit/shopping-list-edit.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipeListComponent } from '../Recipes/recipe-list/recipe-list.component';
+import { RecipeDetailsComponent } from '../Recipes/recipe-details/recipe-details.component';
+import { RecipeItemComponent } from '../Recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from '../ShoppingList/shopping-list.component';
+import { ShoppingListEditComponent } from '../ShoppingList/shopping-list-edit/shopping-list-edit.component';
+import { RecipesComponent } from '../Recipes/recipes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecipeListComponent,
+    HeaderComponent, 
+    RecipeListComponent, 
+    RecipeDetailsComponent, 
     RecipeItemComponent,
-    RecipeDetailsComponent,
     ShoppingListComponent,
     ShoppingListEditComponent,
-    HeaderComponent
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
