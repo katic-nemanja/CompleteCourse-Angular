0d96eb09de29329147d51fbbe701189918f42647
katic-nemanja <katic.nemanja@gmail.com>
2024-11-04 14:11:34 +0100

    gotovo 12. poglavlje

--- 07 - Course Project/src/Recipes/recipe-details/recipe-details.component.ts
+++ 07 - Course Project/src/Recipes/recipe-details/recipe-details.component.ts
@@ -2,11 +2,16 @@ import { Component, Input } from "@angular/core";
 import { Recipe } from "../recipe.model";
 
 @Component({
-    selector:'app-recipe-details',
+    selector: 'app-recipe-details',
     templateUrl: 'recipe-details.component.html',
-    styleUrl:'recipe-details.component.css'
+    styleUrl: 'recipe-details.component.css'
 })
 
-export class RecipeDetailsComponent{
-    @Input({required:true}) inRecept:Recipe = {name:'', description:'', imagePath:''};
+export class RecipeDetailsComponent {
+    @Input({ required: true }) inRecept: Recipe = { name: '', description: '', imagePath: '' };
+
+
+    kliknuto(element: any) {
+        console.log(element.firstChild.text);
+    }
 }
--- 07 - Course Project/src/app/shared/dropdown.directive.ts
+++ 07 - Course Project/src/app/shared/dropdown.directive.ts
@@ -1,10 +1,38 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
+  renderer: Renderer2;
+  elementSelected: ElementRef;
 
-  constructor() { }
+  //prvi nacin
+  counter: number = 1;
+
+
+  constructor(elementSelected: ElementRef, renderer: Renderer2) {
+    this.renderer = renderer;
+    this.elementSelected = elementSelected;
+  }
+
+  //ako je isOpen varijabla "TRUE" bice dodata clasa open, ako ne, uklonice se
+  @HostBinding('class.open') isOpen: boolean = false;
+
+  //prvi nacin
+  // @HostListener('click') mouseClick(eventData: Event) {
+  //   this.counter++;
+  //   if ((this.counter % 2) == 0) {
+  //     this.renderer.addClass(this.elementSelected.nativeElement, 'open');
+  //   }
+  //   else {
+  //     this.renderer.removeClass(this.elementSelected.nativeElement, 'open');
+  //   }
+  // }
+
+  //drugi nacin
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
+  }
 
 }
